e1ddfb3de20c55febc54e658c1258dbcaae3f368
Darshan <109055047+darshangowda11@users.noreply.github.com>
2023-01-24 09:05:44 +0530

    Delete A-2 directory

--- A-2/model/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-async function connection(){
-    await mongoose.connect('mongodb://localhost:/instagram')
-    console.log("mongoose connection is up")
-}
-
-module.exports =connection;
